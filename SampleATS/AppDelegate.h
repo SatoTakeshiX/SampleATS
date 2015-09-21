c0ed75570473b951c48bbe2bd6d474edca8ccfa9
satoutakeshi <sato.takeshi1987@gmail.com>
2015-09-21 23:30:19 +0900

    Initial Commit

---
+++ SampleATS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SampleATS
+//
+//  Created by satoutakeshi on 2015/09/21.
+//  Copyright © 2015年 satoutakeshi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SampleATS/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SampleATS
+//
+//  Created by satoutakeshi on 2015/09/21.
+//  Copyright © 2015年 satoutakeshi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
